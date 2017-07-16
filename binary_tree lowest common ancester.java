e82150d2949a1b1c25f9a2f3efb3073d32a38f7d
Zaw Moe Lwin <zawmoelwin@gmail.com>
2017-07-16 12:06:18 -0700

    Update binary_tree lowest common ancester.java

--- binary_tree lowest common ancester.java
+++ binary_tree lowest common ancester.java
@@ -1,1 +1,13 @@
-
+/*You can consider recursive like asynchronous calls.
+Let common_ancester be NaN
+while common_ancester is NaN do 
+    get the node
+        if node_value equals numbers
+        	return node_value
+        else    
+        	if visit left child && visit right child
+                visit the node, get value
+                assign value
+return the value
+*/    	
+    
