07179a5404990788c47b9ba7505881d0782a1c5d
Zaw Moe Lwin <zawmoelwin@gmail.com>
2017-07-16 19:06:22 -0700

    Update binary_tree lowest common ancester.java

--- binary_tree lowest common ancester.java
+++ binary_tree lowest common ancester.java
@@ -11,3 +11,42 @@ while common_ancester is NaN do
 return the value
 */    	
     
+static Node lca(Node root,int v1,int v2)
+    {
+    // need to check if v1 == v2 and return that node
+    
+    // root is the universal common ancester
+    //Node common_ancester = root;
+    List<Node> v1_path = new ArrayList<>();
+    List<Node> v2_path = new ArrayList<>();
+
+    // there are two strategies.. one is finding the node of one value and from that node on the way back finding the value 2 on child node of that branch.
+    // Another is storing the path of each value and see if the node present in another path.
+    
+    v2_path =  traverse_find(root, v2 , v2_path);
+    v1_path =  traverse_find(root, v1 , v1_path);
+    
+    int i = 0;
+    
+    while (v1_path.get(i)==v2_path.get(i)){
+        i++;
+    }
+              
+   //traverse the tree to find the v1 and v2    
+   // need to check smaller or larger to pass the left and right node
+    return v1_path.get(i-1);
+    }
+
+// assuming the value is present in the tree
+static List traverse_find(Node node, int value, List<Node> path ){
+    
+    path.add(node);
+    if ( node.left != null && node.data < value){
+        path = traverse_find(node.left,value,  path); 
+    } else if (  node.right != null && node.data > value){ 
+        path = traverse_find(node.right, value, path);
+    }
+    
+   return path;
+}
+
